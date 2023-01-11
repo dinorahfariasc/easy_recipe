73e3361293b42605d5ea8c657773ff3d2e965d9c
dinorahfariasc <dinorahfarias@outlook.com>
2023-01-11 20:34:37 -0300

    commit

--- lib/screens/categories_screens.dart
+++ lib/screens/categories_screens.dart
@@ -2,6 +2,7 @@
 
 import 'dart:ui';
 
+import 'package:easy_recipe/models/meal.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,30 @@ class CategoriesScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 80,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: dummyMeals.map((meals) {
+                return Container(
+                  child: Text(meals.label),
+                  margin: EdgeInsets.all(5),
+                  padding: EdgeInsets.all(15),
+                  //color: Colors.white,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.pink,
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
           TextField(
             style: TextStyle(
               color: Colors.white,
@@ -28,16 +53,19 @@ class CategoriesScreen extends StatelessWidget {
               border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(20),
                   borderSide: BorderSide.none),
-              hintText: "pesquisar",
+              hintText: "Saerch your perfect recipe",
               prefixIcon: Icon(Icons.search),
             ),
           ),
           SizedBox(
-            height: 30,
+            height: 10,
           ),
           Expanded(
             child: GridView.count(
               crossAxisCount: 2,
+              crossAxisSpacing: 30,
+              mainAxisSpacing: 20,
+              padding: EdgeInsets.all(20),
               children: dummyCategories.map((cat) {
                 return CategoryItem(cat);
               }).toList(),
